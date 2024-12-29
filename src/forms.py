404f373b770b8d7440f52a92dda4320369a53824
Grisha <gpkrylov@edu.hse.ru>
2024-12-29 10:47:22 +0300

    users and history commands

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class UserForm(StatesGroup):
+    name = State()
     education = State()
-    model = State()
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import sys
 from aiogram import Bot, Dispatcher 
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
@@ -18,39 +18,65 @@ settings = BotSettings()
 
 dp = Dispatcher()
 
+async def send_messages(message: Message, messages: list) -> None:
+    for m in messages:
+        await message.answer(m)
+
 
 @dp.message(CommandStart())
 async def start_handler(message: Message, state: FSMContext) -> None:
-    if message.from_user:
-        settings.users[message.from_user.full_name] = {
-            "model": "",
-            "education": "",
-        }
+    await state.set_state(UserForm.name)
+    await send_messages(message, messages["start"])
+
+
+@dp.message(UserForm.name)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
     await state.set_state(UserForm.education)
-    await message.answer(messages["start"])
+    await send_messages(message, messages["education"])
 
 
 @dp.message(UserForm.education)
 async def process_education(message: Message, state: FSMContext) -> None:
     await state.update_data(education=message.text)
-    await state.set_state(UserForm.model)
-    await message.answer(messages["model"])
-
-
-@dp.message(UserForm.model)
-async def process_mode(message: Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    if message.from_user:
+        id = message.from_user.full_name
+        settings.users[id] = {
+            "name": data["name"],
+            "education": data["education"],
+        }
+        settings.message_history[id] = []
     await state.clear()
-    await message.answer("good")
+
+
+@dp.message(Command("users"))
+async def list_users(message: Message) -> None:
+    await message.answer(str(settings.users))
+
+
+
+@dp.message(Command("history"))
+async def show_history(message: Message) -> None:
+    if message.from_user:
+        id = message.from_user.full_name
+        await message.answer(
+            str(settings.message_history[id])
+        )
 
 
 @dp.message()
-async def echo_handler(message: Message) -> None:
+async def gigachat_handler(message: Message) -> None:
     if not message.text: return
-    
+
+    history = []
+    if message.from_user:
+        history = settings.message_history[message.from_user.full_name]
+
     response = use(
         access_token=settings.gigachat_token,
         model="GigaChat",
-        message_history=settings.message_history,
+        message_history=history,
         proompt=message.text
     )
 
