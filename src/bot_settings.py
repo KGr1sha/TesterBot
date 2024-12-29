4aa5e1b6ee3c91396fe69748a444779dae3ae056
Grisha <gpkrylov@edu.hse.ru>
2024-12-29 10:05:17 +0300

    talking to giga chat

---
+++ src/bot_settings.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from os import getenv
+from gigachat import get_access_token
+
+
+
+class BotSettings:
+    bot_token: str
+    gigachat_token: str
+    message_history: list
+    users: dict
+
+    def __init__(self) -> None:
+        load_dotenv()
+        bt = getenv("BOT_TOKEN")
+        if not bt:
+            raise Exception("Failed to get bot token from env")
+        self.bot_token = bt
+        self.gigachat_token = get_access_token()
+        self.message_history = []
+        self.users = {}
+    
